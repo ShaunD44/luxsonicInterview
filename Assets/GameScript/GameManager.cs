87dc2c59b659184584f670177f03fbf45fd61c0f
shaundowell <shaundowell44@gmail.com>
2021-03-13 09:56:59 -0700

    audio, Debuged code, tested, atempted scrips and annimation

---
+++ Assets/GameScript/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static int itemsCollected = 0;
+    public static int totalItems = 3;
+    public GameObject winScreen;
+    public Text itemDisplay;
+
+    void Update()
+    {
+        if (itemsCollected == totalItems)
+        {
+            //Debug.Log("well done!");
+            winScreen.SetActive(true);
+        }
+        itemDisplay.text = "Items collected" +itemsCollected.ToString();
+    }
+}
